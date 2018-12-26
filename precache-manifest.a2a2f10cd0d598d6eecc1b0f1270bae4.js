1d13c7b4b443b9a9aacbc618b33b65dbaff6bcf5
Khayati Asrani <devinc@TheDen-3.local>
2018-12-27 01:25:48 +0530

    Updates

---
+++ precache-manifest.a2a2f10cd0d598d6eecc1b0f1270bae4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2da7edef590657204f4",
+    "url": "/electric-drums-pwa/static/css/main.a769e21e.chunk.css"
+  },
+  {
+    "revision": "b2da7edef590657204f4",
+    "url": "/electric-drums-pwa/static/js/main.b2da7ede.chunk.js"
+  },
+  {
+    "revision": "38937da122cf19cdba00",
+    "url": "/electric-drums-pwa/static/js/1.38937da1.chunk.js"
+  },
+  {
+    "revision": "f8bfdb3640838307f956",
+    "url": "/electric-drums-pwa/static/js/2.f8bfdb36.chunk.js"
+  },
+  {
+    "revision": "7cd2f925bd7c169d515b",
+    "url": "/electric-drums-pwa/static/js/3.7cd2f925.chunk.js"
+  },
+  {
+    "revision": "e679f02aa8bbf0b2e82e",
+    "url": "/electric-drums-pwa/static/js/4.e679f02a.chunk.js"
+  },
+  {
+    "revision": "cf7de996568fe509e1e7",
+    "url": "/electric-drums-pwa/static/js/5.cf7de996.chunk.js"
+  },
+  {
+    "revision": "a42ce5d54665f4909147",
+    "url": "/electric-drums-pwa/static/js/6.a42ce5d5.chunk.js"
+  },
+  {
+    "revision": "d34ed36d6671cd80dd1b",
+    "url": "/electric-drums-pwa/static/js/runtime~main.d34ed36d.js"
+  },
+  {
+    "revision": "e2e5631369a86db5cbfe30dab116cf0b",
+    "url": "/electric-drums-pwa/index.html"
+  }
+];
